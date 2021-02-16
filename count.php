7f567cda73e784d888d70fc33ed4ef99d8b7e2c3
root <root@zi.id>
2021-02-16 16:32:11 +0700

    testing

--- count.php
+++ count.php
@@ -1,10 +1,12 @@
 <?php
 //connect ke database dengan user dan password admin, ip database dan datanase Trucorp
 $con = mysqli_connect('172.17.0.2', 'admin', 'admin', 'Trucorp');
+
 //pengecekan jika terdapat error pada koneksi ke database
 if (!$con) {
     echo "Error\n"; die('Could not connect: ' . mysql_error());
 }
+
 //jika berhasil maka akan menampilkan semua data yang ada pada database berupa 
 echo "Connected successfully\n";
 $query = "select [count_id] = count(id) from users";
